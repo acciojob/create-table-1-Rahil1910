d7ee4a1e19b1718146d6838a45e5b94b8f11666f
Rahil1910 <rahilrafique1234@gmail.com>
2024-12-31 11:56:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function insert_Row() {
 	const newRow = table.insertRow(0);
 	const cell1 = newRow.insertCell(0);
 	const cell2 = newRow.insertCell(1);
-	cell1.textContent = `New cell1`;
-	cell2.textContent = `New cell2`;
+	cell1.textContent = `New Cell1`;
+	cell2.textContent = `New Cell2`;
 	i++;
 }
 const btn = document.getElementById('button');
